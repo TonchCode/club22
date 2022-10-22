bec24d71289ea4387b7958f539ac715354da37cb
Your Name <tonchonikolov17a@gmail.com>
2022-10-22 11:30:16 +0300

    Week 0
    Hello World

---
+++ Week 0/MyApp2022/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApp2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");    // bez nov red
+            Console.WriteLine("World");// sus nov red
+            Console.ReadLine(); // chete red
+            Console.ReadKey();
+            Console.ReadKey(true);
+            Console.ReadKey();
+        }
+    }
+}

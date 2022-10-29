b81bd8546e616f45b20bd19b08e2286988ab40c6
Your Name <tonchonikolov17a@gmail.com>
2022-10-29 11:26:56 +0300

    Week 1
    If Else And Types

---
+++ Week 1/IfElse/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            //n = int.Parse(Console.ReadLine());
+
+            if(n < 5)
+            {
+                Console.WriteLine("Po malko ot 5");
+            }
+            else if (n == 5)
+            {
+                Console.WriteLine("Ravno na 5");
+            }
+            else
+            {
+                Console.WriteLine("Po golqmo ot 5");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Week 1/Week 1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ctrl + K + C; Ctrl + K + U
+            // int, string, char, bool, double, float
+
+            ///////////////////////////////////// INT /////////////////////////////////////
+            //int a = 10;
+            //int b = 20;
+
+            //a = a + 1;
+            //a++;
+            //++a;
+
+            //a = a + 2;
+            //a += 2;
+
+            //Console.WriteLine(b);
+            //Console.WriteLine(++b);
+            //Console.WriteLine(b++);
+            //Console.WriteLine(b);
+
+            //Console.WriteLine("a = " + a);
+
+            /////////////////////////////////// STRING ////////////////////////////////////////           
+            //char a = 'H';
+            //Console.WriteLine(a + a + a);
+
+            //int welcome = 10;
+
+            //string b = "Hello World (:";
+
+            //Console.WriteLine("String {0} is not equal to int {1} {2} {3};", b, welcome, 10, "afg");
+
+            //Console.WriteLine("String " + b + " is not equal to int " + welcome + ";");
+
+            //Console.WriteLine($"String {b} is not equal to int {welcome}");
+
+            ////// Readline
+            //string input;
+            //input = Console.ReadLine();
+            //Console.WriteLine(input);
+            //Console.ReadLine();
+
+            ////////////////////////////////////// BOOL ///////////////////////////////////
+            //bool ok = true;
+            //bool notOk = false;
+            //Console.WriteLine(ok);
+            //Console.WriteLine(ok + " = ne");
+
+            //if(ok == notOk)
+            //{
+            //    Console.WriteLine();
+            //}
+
+            // Деление /; Умножение *; Събиране +; Изваждане -; Деление с остатък %;
+            // Различни действия при различни типове ^
+
+            ////////////////////////////////// DOUBLE FLOAT /////////////////////////////////
+            //int a = 3;
+            //double b = 2;
+            //Console.WriteLine(a / b);
+            //double c = 3.95472;
+            //Console.WriteLine(a * c);
+            //float f = 3.95472f;
+            //Console.WriteLine(a * f);
+
+            Console.ReadKey(true);
+        }
+    }
+}

ad203f992ce59d7eeb3f12f00eaa9d2f718b768a
Your Name <tonchonikolov17a@gmail.com>
2022-12-17 12:07:31 +0200

    Week 7

---
+++ Week 7/ConsoleColor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleColors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Magenta;
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+
+                        Console.Write(" & ");
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Blue;
+
+                        Console.Write(" % ");
+                    }
+                } 
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week 7/Week 7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_7
+{
+    internal class Program
+    {
+        // number 5
+        // result 25
+        static int Pow(int number, int power)
+        {
+            int result = number;
+            for (int i = 1; i < power; i++)
+            {
+                result = result * number;
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# method: " + Math.Pow(100, 4));
+
+            Console.WriteLine("Our method: " + Pow(100, 4));
+
+            Console.ReadKey(true);
+        }
+    }
+}
